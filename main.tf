737c3a973e472b010144a81e9aae7d924d8c27c5
Sasha Holubenko <165768684+oleksandrh-everc@users.noreply.github.com>
2024-11-25 03:23:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,43 +2,5 @@ provider "aws" {
   region = "us-east-1" # Change this to your desired region
 }
 
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "SashaVPCS"
-  }
-}
 
-resource "aws_subnet" "main_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a" # Change this to your desired AZ
-  tags = {
-    Name = "SashaSubnetS"
-  }
-}
-
-resource "aws_security_group" "allow_all" {
-  vpc_id = aws_vpc.main_vpc.id
-  name   = "sasha_allo_all_sg"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # All traffic
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # All traffic
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "AllowAllSG"
-  }
-}
 
