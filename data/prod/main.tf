2b9394e8ab68c8eff743a0c40bc03010b27c9f04
Sasha Holubenko <165768684+oleksandrh-everc@users.noreply.github.com>
2024-12-04 02:59:44 +0200

    Create main.tf

---
+++ data/prod/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1" # Change this to your desired region
+  profile = "dev"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "SashaDataTest"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a" # Change this to your desired AZ
+  tags = {
+    Name = "SashaDataTest"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  vpc_id = aws_vpc.main_vpc.id
+  name   = "sasha_allo_all_sg3"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # All traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # All traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SashaDataTest"
+  }
+}
